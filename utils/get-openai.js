ec6a6dd9b33c90b9f70193e47d43eda071d96be3
x-dr <79109673+x-dr@users.noreply.github.com>
2022-12-12 16:41:39 +0000

    fix

--- utils/get-openai.js
+++ utils/get-openai.js
@@ -10,30 +10,34 @@ const openai = new OpenAIApi(configuration)
 
 
 const getOpenAiReply = async (prompt) => {
-    const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: prompt,
-        temperature: 0.9,
-        max_tokens: 150,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.6,
-        stop: [' Human:', ' AI:'],
-    })
-    // console.log(response.data);
-    const reply = response.data.choices[0].text
-    return reply
+    try {
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: prompt,
+            temperature: 0.9,
+            max_tokens: 150,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.6,
+            stop: [' Human:', ' AI:'],
+        })
+        // console.log(response.data);
+        const reply = response.data.choices[0].text
+        return reply
+    } catch (e) {
+        console.error(e);
+        // return e
+    }
 }
-
 // getOpenAiReply('删除github上的一次提交')
-const replyMessage = async (mgsfrom,contact, content) => {
+const replyMessage = async (mgsfrom, contact, content) => {
     //   async function replyMessage(contact, content) {
-        const reply = await getOpenAiReply(content);
-        try {
-          await contact.say(`@${mgsfrom} ${reply}`);
-        } catch (e) {
-          console.error(e);
-        }
-      }
+    const reply = await getOpenAiReply(content);
+    try {
+        await contact.say(`@${mgsfrom} ${reply}`);
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 export default replyMessage
