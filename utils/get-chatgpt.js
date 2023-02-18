9bbd3fd02714431521da99d4ea593945788ca129
x-dr <79109673+x-dr@users.noreply.github.com>
2023-02-18 14:03:10 +0000

    docs

--- utils/get-chatgpt.js
+++ utils/get-chatgpt.js
@@ -1,4 +1,7 @@
 import { ChatGPTAPI } from 'chatgpt';
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
 let apiKey = '';
 const api = new ChatGPTAPI({ apiKey: apiKey || process.env.OPENAI_API_KEY });
 
